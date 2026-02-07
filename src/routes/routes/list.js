90f1d0b47fb9c4fd527b5f266810fd279727606b
Josh <joshuazob@gmail.com>
2026-02-06 19:34:25 -0700

    filter fixed

--- src/routes/routes/list.js
+++ src/routes/routes/list.js
@@ -2,13 +2,25 @@ import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/m
 
 export default async (req, res) => {
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
+    let routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
+
+    const {region, season } = req.query;
+
+     if (region && region !== 'all') {
+        routes = routes.filter(route => route.region === region);
+    }
+
+    if (season && season !== 'all') {
+        routes = routes.filter(route => route.bestSeason === season);
+    }
+
 
     res.render('routes/list', { 
         title: 'Scenic Train Routes',
         regions,
         routes,
-        seasons
+        seasons,
+        query: req.query
     });
 };
